5e9bba61dd3e5ca0a922f8ad7697c92bec3e207d
MehmetUlDG <mehmetuludag2021@outlook.com>
2025-12-12 23:47:36 +0300

    latest commit

--- movieclub/src/components/common/EmptyState.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const EmptyState = ({ message }) => (
-  <div style={{ padding: '2rem', textAlign: 'center' }}>
-    <p>{message || "Gösterilecek sonuç bulunamadı."}</p>
-  </div>
-);
-
-export default EmptyState;
--- movieclub/src/components/common/Spinner.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Spinner = () => (
-  <div style={{ padding: '2rem', textAlign: 'center' }}>
-    <p>Yükleniyor...</p>
-  </div>
-);
-
-export default Spinner;
